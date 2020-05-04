70f93c071dc423176ec371a40f01e3f851e4098d
foxett <foxett@yandex.ru>
2020-05-05 02:37:59 +0300

    refactoring and fixes select project page

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,6 @@
 
 /*hover for menu-desktop*/
-$('.menu-icon').hover(
+$('.menu-icon, .menu-desktop-content').hover(
     function(){
         $('.menu-desktop-content').removeClass('d-none');
         $('.menu-desktop-content').addClass('d-inline-flex');
@@ -14,25 +14,6 @@ $('.menu-icon').hover(
         $('.menu-desktop-content').removeClass('d-inline-flex');
         $('.menu-desktop-content').addClass('d-none');
         $('.header-title').removeClass('d-none');
-    });
-
-
-$('.menu-desktop-content').hover(
-    function(){
-        $(this).removeClass('d-none');
-        $(this).addClass('d-inline-flex');
-
-        if($(document).width() < 1360){
-            $('.header-title').addClass('d-none');
-        }
-
-    },
-    function(){
-        $(this).removeClass('d-inline-flex');
-        $(this).addClass('d-none');
-
-        $('.header-title').removeClass('d-none');
-
     });
 
 /*hover for filter-desktop*/
@@ -53,19 +34,6 @@ $('.filter-desktop, .menu-filter-desktop__content').hover(
         $('.header-title').removeClass('d-none');
     });
 
-//TODO: Remove this duplicate code after release
-// $('.menu-filter-desktop__content').hover(
-//     function(){
-//         $(this).removeClass('d-none');
-//         $(this).addClass('d-inline-flex');
-//
-//     },
-//     function(){
-//         $(this).removeClass('d-inline-flex');
-//         $(this).addClass('d-none');
-//     });
-
-
 /*ховер на картинку preview-projects*/
 
 // $(document).ready(function(){
@@ -82,42 +50,37 @@ $('.filter-desktop, .menu-filter-desktop__content').hover(
 
 
 //инициализация слайдеров на странице projects education bureau
-function slideInit(){
-    $('.education-slide').slick({});
-    $('.bureau-mobile-slide').slick({});
-    $('.education-mobile-slide').slick({});
-    $('.projest-slide').slick({});
-}
 
-/*вычисление высоты блока с описанием проекта*/
 $(document).ready(function(){
-    function descriptionPageHeight (){
-    try{
-        let headerContainer = document.querySelector('.header-container');
-        let descriptionPageContainer = document.querySelector('.description-page__container');
-        let descriptionPage = document.querySelector('.description-page');
-        let headerBlockHeight = headerContainer.offsetHeight;
-        let headerDescriptionPage = descriptionPageContainer.style.height = descriptionPage.clientHeight - headerBlockHeight + 'px';
 
-        return headerDescriptionPage;
-    }catch(e){
-        console.log(e.message)
+    /*для преобразования меню в крестик при клике*/
+    function openMobileMenu(x) {
+        x.classList.toggle("change");
     }
 
+    function slideInit(){
+        if($('body').hasClass('education')){
+            $('.education-slide').slick({});
+            $('.education-mobile-slide').slick({});
+        }
 
+        if($('body').hasClass('bureau')){
+            $('.bureau-mobile-slide').slick({});
+        }
+
+       if($('body').hasClass('select-project-page')){
+           $('.projest-slide').slick({});
+       }
 
     }
-    //descriptionPageHeight ();
 
     function isotopeInit(){
-        // init Isotope
 
         var $grid = $('.preview-projects__content').isotope({
             itemSelector: '.preview-projects__item',
             layoutMode: 'fitRows',
             filter: '*'
         });
-
 
         $('.menu-filter-desktop__content').on( 'click', 'a', function() {
             var filterValue = $( this ).attr('data-filter');
@@ -161,10 +124,17 @@ $(document).ready(function(){
 
         }, 1000);
 
-
             slideInit();
+    }
 
-
+    function heightProjectSlide() {
+        if ($(window).height() <= '1000'){
+            $('.projest-slide').css('height', '450px');
+        }
+        else   {
+            $('.projest-slide').css('height', '600px');
+        }
+        return;
     }
 
     $('.preloader-page').click(function(){
@@ -175,12 +145,146 @@ $(document).ready(function(){
 
     });
 
-    setTimeout(" $('.preloader-page').click()", 2000);
+    setTimeout(function(){
+        $('.preloader-page').click();
+    }, 2000);
+
+    $('.menu-icon').click(function(){
+        $('body').toggleClass('mob-menu-active');
+    });
+
+
+    $('.mobile-menu a').on('click', function(){
+        $('body').removeClass('mob-menu-active');
+    });
+
+    /*Открытие блока профиля учредителя*/
+
+    $('#zimailo-link, #liachenko-link').click(function(){
+        var block = $(this).data('block');
+        $('#' + block).toggleClass('modal-profile-active');
+    });
+
+    /*Закрытие блока профиля учредителя*/
+
+    $('.menu-icon_mobile').click(function(){
+        $(this).closest('.modal-profile').removeClass('modal-profile-active');
+    });
+
+    $('.btn-up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+    /**
+     * Main page
+     */
+
+    if($('body').hasClass('main-page')){
+        // set the starting position of the cursor outside of the screen
+        let clientX = -100;
+        let clientY = -100;
+        const innerCursor = document.querySelector(".mouse-cursor");
+
+        const initCursor = () => {
+            // add listener to track the current mouse position
+            document.querySelector('.preloader-page').addEventListener("mousemove", e => {
+                clientX = e.clientX;
+                clientY = e.clientY;
+            });
+
+            // transform the innerCursor to the current mouse position
+            // use requestAnimationFrame() for smooth performance
+            const render = () => {
+                if(!innerCursor){
+                    return;
+                }
+                innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
+                // if you are already using TweenMax in your project, you might as well
+                // use TweenMax.set() instead
+                // TweenMax.set(innerCursor, {
+                //   x: clientX,
+                //   y: clientY
+                // });
+
+                requestAnimationFrame(render);
+            };
+            requestAnimationFrame(render);
+        };
+
+        initCursor();
+
+        $('.cursor_triger').hover(function() {
+            cursorAddClass($(this).data('cursor'))
+        }, function() {
+            cursorRemoveClass($(this).data('cursor'))
+        });
+
+        function cursorAddClass(className) {
+            $(innerCursor).addClass(className);
+        }
+
+        function cursorRemoveClass(className) {
+            $(innerCursor).removeClass(className);
+        }
+    }
+
+
+    /**
+     * Bureau page
+     */
+    if($('body').hasClass('bureau')){
+
+        if ($(window).width() <= '992'){
+            $('body').addClass('bureau-mobile');
+        }else {
+            $('body').removeClass('bureau-mobile');
+        }
+
+        $(window).resize(function() {
+            if ($(window).width() <= '992'){
+                $('body').addClass('bureau-mobile');
+            }else {
+                $('body').removeClass('bureau-mobile');
+            }
+        });
+
+        try{
+            $(".onepage-wrapper").onepage_scroll({
+                sectionContainer: "section",
+                responsiveFallback: false,
+                loop: true
+            });
+            $(document).on('mousewheel DOMMouseScroll', '.awards-page', function(event) {
+                event.stopPropagation();
+            });
+        }catch(e){
+            console.log(e.message);
+        }
+    }
 
     /**
      * Select  project page
      */
     if($('body').hasClass('select-project-page')){
+
+        /*вычисление высоты блока с описанием проекта*/
+        function descriptionPageHeight (){
+            try{
+                let headerContainer = document.querySelector('.header-container');
+                let descriptionPageContainer = document.querySelector('.description-page__container');
+                let descriptionPage = document.querySelector('.description-page');
+                let headerBlockHeight = headerContainer.offsetHeight;
+                let headerDescriptionPage = descriptionPageContainer.style.height = descriptionPage.clientHeight - headerBlockHeight + 'px';
+
+                return headerDescriptionPage;
+            }catch(e){
+                console.log(e.message)
+            }
+        }
+
         $(document).keydown(function(e) {
             switch (e.which) {
                 case 37:
@@ -202,28 +306,45 @@ $(document).ready(function(){
 
         $('.btn-details').click(function(){
             offPreloader('select-project', 'description-page');
-            descriptionPageHeight ();
+            descriptionPageHeight();
+        });
+
+        $(window).resize(function() {
+            if ($(window).height() <= '1000'){
+                $('.projest-slide').css('height', '450px');
+                $('.projest-slide img').css('height', '450px');
+            }
+            else   {
+                $('.projest-slide').css('height', '600px');
+                $('.projest-slide img').css('height', '600px');
+            }
+            try{
+                $('.projest-slide').slick({});
+            }catch(e){
+
+            }
+
+        });
+
+        $('.projest-slide').mousewheel(function(e) {
+            e.preventDefault();
+
+            if (e.deltaY < 0) {
+                $(this).slick('slickNext');
+            } else {
+                $(this).slick('slickPrev');
+            }
+        });
+
+        $('.projest-slide').on('afterChange', function(event, slick, currentSlide, nextSlide){
+            let slideNum = currentSlide < 10 ? '0' + (currentSlide + 1) : currentSlide;
+            $('.slide-number .active-number').html(slideNum);
         });
     }
 
     /**********************************************/
 });
 
-$(document).ready(function(){
-    try{
-        $(".onepage-wrapper").onepage_scroll({
-            sectionContainer: "section",
-            responsiveFallback: false,
-            loop: true
-        });
-        $(document).on('mousewheel DOMMouseScroll', '.awards-page', function(event) {
-            event.stopPropagation();
-        });
-    }catch(e){
-        console.log(e.message);
-    }
-
-});
 
 /*for menu на экранах меньше 1360*/
 
@@ -238,83 +359,15 @@ $(document).ready(function(){
 //     });
 // });
 
-function heightProjectSlide() {
-    if ($(window).height() <= '1000'){
-        $('.projest-slide').css('height', '450px');
-    }
-    else   {
-        $('.projest-slide').css('height', '600px');
-    }
-    return;
-}
 
 
-$(window).resize(function() {
-    if ($(window).height() <= '1000'){
-        $('.projest-slide').css('height', '450px');
-        $('.projest-slide img').css('height', '450px');
-    }
-    else   {
-        $('.projest-slide').css('height', '600px');
-        $('.projest-slide img').css('height', '600px');
-    }
-    try{
-        $('.projest-slide').slick({});
-    }catch(e){
-
-    }
-
-});
-
-$(document).ready(function(){
-    $('.menu-icon').click(function(){
-        $('body').toggleClass('mob-menu-active');
-    });
 
 
-    $('.mobile-menu a').on('click', function(){
-        $('body').removeClass('mob-menu-active');
-    });
 
-    /*Открытие блока профиля учредителя*/
 
-    $('#zimailo-link, #liachenko-link').click(function(){
-        var block = $(this).data('block');
-        $('#' + block).toggleClass('modal-profile-active');
-    });
 
-    /*Закрытие блока профиля учредителя*/
 
-     $('.menu-icon_mobile').click(function(){
-         $(this).closest('.modal-profile').removeClass('modal-profile-active');
-     });
 
-    $('.btn-up').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 500);
-        return false;
-    });
-
-});
-/*для преобразования меню в крестик при клике*/
-function openMobileMenu(x) {
-    x.classList.toggle("change");
-}
-$(document).ready(function(){
-    if ($(window).width() <= '992'){
-        $('body').addClass('bureau-mobile');
-    }else {
-        $('body').removeClass('bureau-mobile');
-    }
-});
-$(window).resize(function() {
-    if ($(window).width() <= '992'){
-        $('body').addClass('bureau-mobile');
-    }else {
-        $('body').removeClass('bureau-mobile');
-    }
-});
 
 
 //определение высоты слайдера на странице бюро в мобилке
@@ -331,55 +384,4 @@ $(window).resize(function() {
 //    getSlideHeight();
 // });
 
-
-$(document).ready(function(){
-    // set the starting position of the cursor outside of the screen
-    let clientX = -100;
-    let clientY = -100;
-    const innerCursor = document.querySelector(".mouse-cursor");
-
-    const initCursor = () => {
-        // add listener to track the current mouse position
-        document.querySelector('.preloader-page').addEventListener("mousemove", e => {
-            clientX = e.clientX;
-            clientY = e.clientY;
-        });
-
-        // transform the innerCursor to the current mouse position
-        // use requestAnimationFrame() for smooth performance
-        const render = () => {
-            if(!innerCursor){
-                return;
-            }
-            innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
-            // if you are already using TweenMax in your project, you might as well
-            // use TweenMax.set() instead
-            // TweenMax.set(innerCursor, {
-            //   x: clientX,
-            //   y: clientY
-            // });
-
-            requestAnimationFrame(render);
-        };
-        requestAnimationFrame(render);
-    };
-
-    initCursor();
-
-    $('.cursor_triger').hover(function() {
-        cursorAddClass($(this).data('cursor'))
-    }, function() {
-        cursorRemoveClass($(this).data('cursor'))
-    });
-
-    function cursorAddClass(className) {
-        $(innerCursor).addClass(className);
-    }
-
-    function cursorRemoveClass(className) {
-        $(innerCursor).removeClass(className);
-    }
-
-
-});
 
